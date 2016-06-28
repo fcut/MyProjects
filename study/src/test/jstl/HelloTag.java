b0ed5478f1bb089b737b15f7fe866c870608cc3f
unknown <>
2016-06-28 13:42:24 +0800

    Initial commit

---
+++ study/src/test/jstl/HelloTag.java
@@ -0,0 +1,15 @@
+package test.jstl;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+
+/**
+ * Created by dejun on 2016-06-28.
+ */
+public class HelloTag extends SimpleTagSupport{
+    @Override
+    public void doTag() throws JspException, IOException {
+        super.doTag();
+    }
+}
